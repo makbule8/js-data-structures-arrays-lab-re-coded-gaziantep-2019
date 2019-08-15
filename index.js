e5d88f02aa6d93f89929332cf2b9e6e9a1f9f827
makbule özbay <makbuleozbay8@gmail.com>
2019-08-15 12:09:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,25 +8,26 @@ function destructivelyPrependKitten(name) {
   return name
 }
 function destructivelyRemoveLastKitten() {
-  return kittens.pop()
+  return kittens.pop();
 }
 function destructivelyRemoveFirstKitten() {
-  return kittens.shift()
+  return kittens.shift();
 }
 function appendKitten(name) {
-  var kittens = ['Milo', 'Otis', 'Garfield']
-  kittens = [...kittens, 'Broom']
-  return kittens
+  var kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens = [...kittens, 'Broom'];
+  return kittens;
 }
  function prependKitten(name) {
-  var kittens = ['Milo', 'Otis', 'Garfield']
-  kittens = ['Arnold', ...kittens]
-  return kittens
+  var kittens = ['Milo', 'Otis', 'Garfield'];
+  kittens = ['Arnold', ...kittens];
+  return kittens;
 }
 
 function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
+  return kittens.slice(0, kittens.length - 1);
 }
+
 function removeFirstKitten() {
-  return kittens.slice(1)
+  return kittens.slice(1);
 } 
